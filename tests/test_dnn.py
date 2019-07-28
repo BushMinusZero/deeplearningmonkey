1f7d97341dd30e01f9e4d88dd55bdac4afd90a5b
Nate Bush <nate_bush@nates-air.attlocal.net>
2019-07-28 11:04:52 -0700

    forward and backward propogation

--- tests/test_dnn.py
+++ tests/test_dnn.py
@@ -1,10 +1,16 @@
 import os
 import sys
-from dnn import initialize_parameters
+from dnn import *
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 parameters = initialize_parameters(3,2,1)
 
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+parameters = initialize_parameters_deep([5,4,3])
 print("W1 = " + str(parameters["W1"]))
 print("b1 = " + str(parameters["b1"]))
 print("W2 = " + str(parameters["W2"]))
